76f701a8312057523a940de0d3432deb553e3ba0
Henrique Reis <77730247+RickAllstar@users.noreply.github.com>
2024-02-26 14:38:54 -0300

    Update atualizar php e html

---
+++ Sistema-escolar/atualizar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excluir</title>
+</head>
+<body>
+<?php
+//estabelece conexão com o servidor e BD 
+    include_once('connection.php');
+
+//recebe dados do formulario
+    $matricula = $_POST['matricula'];
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $turma = $_POST['turma'];
+    $senha = $_POST['senha'];
+
+    $atualizar = mysqli_querry($connection, "UPDATE cadastrar_aluno 
+                                SET matricula = '$matricula'
+                                nome = '$nome'
+                                cpf = '$cpf'
+                                turma = '$turma'
+                                senha = '$senha'
+                                 Where cadastro_aluno = '$cadastro_aluno'");
+    echo "Aluno atualizado com sucesso";
+
+?>
+</body>
+</html>
